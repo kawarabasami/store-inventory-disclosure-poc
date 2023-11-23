13d1ccd453defb03975295587efa1a561859cf29
m-nakamine <kawarabasami.dev@gmail.com>
2023-11-23 15:27:49 +0900

    Insert用処理を共通化

--- supabase/functions/products/index.ts
+++ supabase/functions/products/index.ts
@@ -1,27 +1,9 @@
 import * as postgres from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 import { Product } from "../../domain/product/types.ts";
-import { QueryArguments } from "https://deno.land/x/postgres@v0.17.0/query/query.ts";
-
-function makeValuesStringPlaceholders(recordCount: number) {
-  const values = [];
-  for (let i = 0; i < recordCount; i++) {
-    const baseNum = i * 3;
-    values.push(`($${baseNum + 1}, $${baseNum + 2}, $${baseNum + 3})`);
-  }
-  return values.join(",");
-}
-
-function makeArgs(data: Product[]): QueryArguments {
-  const pArray = data.map((d) => {
-    return [d.productId, d.productCategoryId, d.productName];
-  });
-  return pArray.flat();
-}
-
-function makeValuesString(data: Product) {
-  const { productId: pId, productCategoryId: pcId, productName: pName } = data;
-  return `('${pId}', '${pcId}', '${pName}')`;
-}
+import {
+  makeArgs,
+  makeValuesStringPlaceholders,
+} from "../../utils/sqlUtils.ts";
 
 // Get the connection string from the environment variable "SUPABASE_DB_URL"
 const databaseUrl = Deno.env.get("SUPABASE_DB_URL")!;
@@ -40,7 +22,7 @@ Deno.serve(async (req) => {
     INSERT INTO products 
       (product_id, product_category_id, product_name)
     VALUES
-      ${makeValuesStringPlaceholders(inputs.length)}
+      ${makeValuesStringPlaceholders(inputs.length, 3)}
     ON CONFLICT (product_id)
     DO UPDATE SET
       product_category_id = excluded.product_category_id
@@ -52,7 +34,11 @@ Deno.serve(async (req) => {
       // Run a query
       const result = await connection.queryArray({
         text: query,
-        args: makeArgs(inputs),
+        args: makeArgs(inputs, [
+          "productId",
+          "productCategoryId",
+          "productName",
+        ]),
       });
 
       // Encode the result as pretty printed JSON
