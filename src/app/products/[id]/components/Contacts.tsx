6a45d9e02d268adc2ca9af068d237034176a0a51
m-nakamine <kawarabasami.dev@gmail.com>
2023-12-10 22:59:34 +0900

    fix contact form

--- src/app/products/[id]/components/Contacts.tsx
+++ src/app/products/[id]/components/Contacts.tsx
@@ -5,16 +5,20 @@ function Contacts() {
   return (
     <div className="bg-white py-6 sm:py-8 lg:py-12 w-3/4">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <div className="flex flex-col overflow-hidden rounded-lg bg-gray-200 sm:flex-row md:h-80">
+        <div className="flex flex-col overflow-hidden rounded-lg bg-gray-200 sm:flex-row md:h-60">
           {/* image - start */}
-          <div className="order-first h-48 w-full bg-gray-300 sm:order-none sm:h-auto sm:w-1/2 lg:w-2/5 flex items-center justify-center">
-            <IconifyWrapper icon="mynaui:telephone-call" size="10rem" />
+          <div className="order-first h-48 w-full bg-gray-100 sm:order-none sm:h-auto sm:w-1/2 lg:w-2/5 flex items-center justify-center">
+            <IconifyWrapper
+              icon="ic:round-phone-in-talk"
+              size="10rem"
+              color="#3cb371"
+            />
           </div>
           {/* image - end */}
 
           {/* content - start */}
-          <div className="flex w-full flex-col p-4 sm:w-1/2 sm:p-8 lg:w-3/5">
-            <h2 className="mb-4 text-xl font-bold text-gray-800 md:text-2xl lg:text-4xl">
+          <div className="flex w-full flex-col p-4 sm:w-1/2 sm:p-8 lg:w-3/5 justify-center">
+            <h2 className="mb-4 text-lg font-bold text-gray-800 md:text-xl lg:text-2xl">
               お問合せはこちら
             </h2>
 
@@ -25,11 +29,11 @@ function Contacts() {
             </p>
 
             <div className="flex flex-col gap-2">
-              <a className="inline-block rounded-lg bg-white px-8 py-3 text-center text-sm font-semibold text-gray-800 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:text-base">
-                TEL: xxx-xxx-xxxx
-              </a>
-              <a className="inline-block rounded-lg bg-white px-8 py-3 text-center text-sm font-semibold text-gray-800 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:text-base">
-                FAX: xxx-xxx-xxxx
+              <a
+                className="inline-block rounded-lg bg-white px-8 py-3 text-center text-sm font-semibold text-gray-800 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:text-base"
+                href="tel:098-966-2125"
+              >
+                TEL: 098-966-2125
               </a>
             </div>
           </div>
