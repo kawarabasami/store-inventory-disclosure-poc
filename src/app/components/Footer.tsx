6298e44e167b422035fcd65a2f1ed03055d38d8b
m-nakamine <kawarabasami.dev@gmail.com>
2023-12-16 22:31:26 +0900

    add opening time

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,26 +4,6 @@ import React from "react";
 
 function Footer() {
   return (
-    // <div className="bg-white pt-4 sm:pt-10 lg:pt-12">
-    //   <footer className="mx-auto max-w-screen-2xl px-4 md:px-8">
-    //     <div className="flex flex-col items-center justify-end gap-4 border-t border-b py-6 md:flex-row">
-    //       {/* nav - start */}
-    //       <nav className="flex flex-wrap justify-center gap-x-4 gap-y-2 md:justify-start md:gap-6">
-    //         <Link
-    //           href="https://lifepoint-okinawa.com/"
-    //           target="_blank"
-    //           className="text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600"
-    //         >
-    //           ライフポイント 公式サイト
-    //         </Link>
-    //       </nav>
-    //       {/* nav - end */}
-    //     </div>
-    //     <div className="py-8 text-center text-sm text-gray-400">
-    //       © 2023 - リフォームのライフポイント All rights reserved.
-    //     </div>
-    //   </footer>
-    // </div>
     <div className="pt-4 sm:pt-10 lg:pt-12 border-t">
       <footer className="mx-auto max-w-screen-2xl px-4 md:px-8">
         <div className="">
@@ -43,6 +23,9 @@ function Footer() {
               沖縄県国頭郡恩納村字恩納6084
               <br />
               EPD合同会社
+              <br />
+              営業時間: 9:00-18:00
+              <br /> (日曜・年末年始除く)
             </p>
             <p>
               <Link
